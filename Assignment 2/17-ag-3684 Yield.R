3181f23c142f5df48b7674cd635199a9aadb834e
Sidra Sarwar <43807412+sidra888@users.noreply.github.com>
2018-10-08 10:00:22 +0500

    Update 17-ag-3684 Yield.R

--- Assignment 2/17-ag-3684 Yield.R
+++ Assignment 2/17-ag-3684 Yield.R
@@ -4,8 +4,10 @@
 a <-read.delim(file.choose(),header=T)
 a
 attach(a)
+
 # Multiple Linear Regression Example 
 fit <- lm(Yield ~ Rainfall + Fert)
+
 #MEAN OF Yield, Rainfall & Yield
 u=mean(Yield)
 l=mean(Rainfall)
@@ -13,8 +15,10 @@ d=mean(Fert)
 print(u)
 print(l)
 print(d)
+
 #Value OF Mean Square Error
 (summary(fit)$sigma)**2
+
 #STANDARD ERRORS OF COEFFICIENTS
 summary(fit)$coef[,2] 
 
@@ -25,27 +29,36 @@ h=slp <-  coef(fit)[3]
 print(s)
 print(a)
 print(h)
+
 #FITTED YIELD,
 z=(a+(s*Rainfall)+(h*Fert))
 print(z)
+
 #TOTAL Sum of Square
 k=sum((Yield-u)^2)
 print(k)
+
 #REGRESSION Sum of Square
 p=sum((z-u)^2)
 print(p)
+
 #ERROR Sum of Square
 g=sum((Yield-z)^2)
 print(g)
+
 #R SQUARE COefficient of Determination
 lk=(p/k)
 print(lk)
+
 #Analysis of variance
 anova(fit)
+
 #Confidence Interval 
 confint(fit)
+
 #Summary of whole result
 summary(fit) # show results
+
 #GRaph of the MLR
 plot(fit,main="Scatterplot")
 #ONE SAMPLE T TEST
