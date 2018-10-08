f883bd87d680fc120016239c4040b54601bf2fe4
Sidra Sarwar <43807412+sidra888@users.noreply.github.com>
2018-10-08 09:56:49 +0500

    Add files via upload

---
+++ 17-ag-3684 X1.R
@@ -0,0 +1,57 @@
+# MS(CS)  2017-AG-3684 
+# ASSIGNMENT NO 2
+#Get data from excel file
+a <-read.delim(file.choose(),header=T)
+a
+attach(a)
+# Multiple Linear Regression Example 
+it <- lm(Y ~ X1 + X2)
+
+#Mean of Y,X1,X2
+u=mean(Y)
+l=mean(X1)
+d=mean(X2)
+print(u)
+print(l)
+print(d)
+#Value of Mean Square Error
+(summary(it)$sigma)**2
+#STANDARD ERRORS OF COEFFICIENTS
+summary(it)$coef[,2] 
+#GET VALUES OF SLOPE AND INTERCEPT
+
+a=intcp <- coef(it)[1] 
+s=slp <-  coef(it)[2] 
+h=slp <-  coef(it)[3] 
+print(s)
+print(a)
+print(h)
+#FITTED Y,
+z=(a+(s*X1)+(h*X2))
+print(z)
+#TOTAL Sum of Square
+k=sum((Y-u)^2)
+print(k)
+#REGRESSION Sum of Square
+p=sum((z-u)^2)
+print(p)
+#ERROR Sum of Square
+g=sum((Y-z)^2)
+print(g)
+#R SQUARE COfficient of DETERMINATION
+yt=p/k
+print(yt)
+#Confidence Interval 
+confint(it)
+#Summary of Whole Data
+summary(it) 
+#Graph of MLR
+plot(it,main="Scatterplot")
+#ANOVA TABLE
+anova(it)
+#ONE SAMPLE T TEST
+t.test(X1, mu = 0, alternative = "two.sided")
+#ONE SAMPLE T TEST
+t.test(X2, mu = 0, alternative = "two.sided")
+#WELCH SAMPLE T TEST
+t.test(X1,X2)
---
+++ 17-ag-3684 Yield.R
@@ -0,0 +1,56 @@
+# MS(CS)  2017-AG-3684 
+# ASSIGNMENT NO 2
+#Get data from excel file
+a <-read.delim(file.choose(),header=T)
+a
+attach(a)
+# Multiple Linear Regression Example 
+fit <- lm(Yield ~ Rainfall + Fert)
+#MEAN OF Yield, Rainfall & Yield
+u=mean(Yield)
+l=mean(Rainfall)
+d=mean(Fert)
+print(u)
+print(l)
+print(d)
+#Value OF Mean Square Error
+(summary(fit)$sigma)**2
+#STANDARD ERRORS OF COEFFICIENTS
+summary(fit)$coef[,2] 
+
+#GET VALUES OF SLOPE AND INTERCEPT
+a=intcp <- coef(fit)[1] 
+s=slp <-  coef(fit)[2] 
+h=slp <-  coef(fit)[3] 
+print(s)
+print(a)
+print(h)
+#FITTED YIELD,
+z=(a+(s*Rainfall)+(h*Fert))
+print(z)
+#TOTAL Sum of Square
+k=sum((Yield-u)^2)
+print(k)
+#REGRESSION Sum of Square
+p=sum((z-u)^2)
+print(p)
+#ERROR Sum of Square
+g=sum((Yield-z)^2)
+print(g)
+#R SQUARE COefficient of Determination
+lk=(p/k)
+print(lk)
+#Analysis of variance
+anova(fit)
+#Confidence Interval 
+confint(fit)
+#Summary of whole result
+summary(fit) # show results
+#GRaph of the MLR
+plot(fit,main="Scatterplot")
+#ONE SAMPLE T TEST
+t.test(Fert, mu = 25, alternative = "two.sided")
+#ONE SAMPLE T TEST
+t.test(Rainfall, mu = 25, alternative = "two.sided")
+#WELCH SAMPLE T TEST
+t.test(Rainfall,Fert)
