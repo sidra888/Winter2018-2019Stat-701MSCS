f68889b1ae967eccbe83274417fe7d472dd1b61c
Sidra Sarwar <43807412+sidra888@users.noreply.github.com>
2018-10-08 10:01:51 +0500

    Update 17-ag-3684 X1.R

--- Assignment 2/17-ag-3684 X1.R
+++ Assignment 2/17-ag-3684 X1.R
@@ -14,12 +14,14 @@ d=mean(X2)
 print(u)
 print(l)
 print(d)
+
 #Value of Mean Square Error
 (summary(it)$sigma)**2
+
 #STANDARD ERRORS OF COEFFICIENTS
 summary(it)$coef[,2] 
-#GET VALUES OF SLOPE AND INTERCEPT
 
+#GET VALUES OF SLOPE AND INTERCEPT
 a=intcp <- coef(it)[1] 
 s=slp <-  coef(it)[2] 
 h=slp <-  coef(it)[3] 
@@ -29,26 +31,35 @@ print(h)
 #FITTED Y,
 z=(a+(s*X1)+(h*X2))
 print(z)
+
 #TOTAL Sum of Square
 k=sum((Y-u)^2)
 print(k)
+
 #REGRESSION Sum of Square
 p=sum((z-u)^2)
 print(p)
+
 #ERROR Sum of Square
 g=sum((Y-z)^2)
 print(g)
+
 #R SQUARE COfficient of DETERMINATION
 yt=p/k
 print(yt)
+
 #Confidence Interval 
 confint(it)
+
 #Summary of Whole Data
 summary(it) 
+
 #Graph of MLR
 plot(it,main="Scatterplot")
+
 #ANOVA TABLE
 anova(it)
+
 #ONE SAMPLE T TEST
 t.test(X1, mu = 0, alternative = "two.sided")
 #ONE SAMPLE T TEST
